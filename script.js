75ca7c51d35365bdb39ca6e1c9dddbedec5d857d
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-08-22 17:24:22 +0330

    Add(class, div): answer div and class
    create six answer divs and give them answer class name.

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ answerBox.id = "answerBox";
 
 // append answerBox div to general div
 questions.append(answerBox);
+
+// create a answer divs
+const answer1 = document.createElement("div");
+const answer2 = document.createElement("div");
+const answer3 = document.createElement("div");
+const answer4 = document.createElement("div");
+const answer5 = document.createElement("div");
+const answer6 = document.createElement("div");
+
+// give class name to answer divs
+answer1.className = "answer";
+answer2.className = "answer";
+answer3.className = "answer";
+answer4.className = "answer";
+answer5.className = "answer";
+answer6.className = "answer";
